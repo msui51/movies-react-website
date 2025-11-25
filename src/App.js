4aeff1884a4db9fa233b52f8d6326557fe8d59ff
msui51 <maria.sui51@gmail.com>
2025-11-25 12:24:18 -0500

    installed gh-page

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   
 
   return (
-    <Router>
+    <Router basename='/movies-react-website'>
       <Routes>
         <Route path="/" element={<Home fetchMovieData={fetchMovieData}/>} />
         <Route path="/movies" element={<Movies movies={movies} fetchMovieData={fetchMovieData} setMovies={setMovies} />} />
